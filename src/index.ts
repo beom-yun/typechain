c486f8178664229f54817433f406dec08b78b702
beom-yun <scds0326@naver.com>
2022-04-30 14:05:15 +0900

    JSDoc 사용 : JS 코드를 TS에서 사용할 수 있는 방법 / js 코드를 수정하지 않고 코멘트 만으로도 사용 가능

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
 // myPackage 모듈을 마치 npm install로 다운받은 모듈인 것 마냥 사용
-import { init, exit } from "myPackage"
-
-init({
-    url: "true"
-})
+import { init, exit } from "./myPackage"
--- src/myPackage.js
+++ src/myPackage.js
@@ -1,6 +1,22 @@
+// JSDoc : JS 코드를 TS에서 사용하고 싶을 때, JS코드를 수정하지 않고 코멘트 만으로도 TS에서 사용가능하도록 함
+
+// @ts-check
+/**
+ * 프로젝트 초기화
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns boolean
+ */
 export function init(config) {
     return true
 }
+
+/**
+ * 프로그램 종료
+ * @param {number} code
+ * @returns number
+ */
 export function exit(code) {
     return code + 1
 }
